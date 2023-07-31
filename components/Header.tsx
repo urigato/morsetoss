5ace0bd63904d6f4759797dacc24216aec90ad93
gychoi <gychoi@gatolab.com>
2023-08-01 00:56:01 +0900

    main 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,13 +11,13 @@ export default function Header({
   return (
     <header className="flex flex-col xs:flex-row justify-between items-center w-full mt-3 border-b pb-7 sm:px-4 px-2 border-gray-500 gap-2">
       <Link href="/" className="flex space-x-2">
-        <Image
-          alt="header text"
-          src="/bed.svg"
-          className="sm:w-10 sm:h-10 w-9 h-9"
-          width={24}
-          height={24}
-        />
+        {/*<Image*/}
+        {/*  alt="header text"*/}
+        {/*  src="/bed.svg"*/}
+        {/*  className="sm:w-10 sm:h-10 w-9 h-9"*/}
+        {/*  width={24}*/}
+        {/*  height={24}*/}
+        {/*/>*/}
         <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">
           Morse Toss
         </h1>
@@ -30,15 +30,16 @@ export default function Header({
           {/*>*/}
           {/*  <div>Dashboard</div>*/}
           {/*</Link>*/}
-          <Link
-            href="/buy-credits"
-            className="border-r border-gray-300 pr-4 flex space-x-2 hover:text-blue-400 transition"
-          >
-            <div>Buy Credits</div>
-            <div className="text-blue-500 bg-blue-200 rounded-full px-2 text-xs flex justify-center items-center font-bold">
-              New
-            </div>
-          </Link>
+
+          {/*<Link*/}
+          {/*  href="/buy-credits"*/}
+          {/*  className="border-r border-gray-300 pr-4 flex space-x-2 hover:text-blue-400 transition"*/}
+          {/*>*/}
+          {/*  <div>Buy Credits</div>*/}
+          {/*  <div className="text-blue-500 bg-blue-200 rounded-full px-2 text-xs flex justify-center items-center font-bold">*/}
+          {/*    New*/}
+          {/*  </div>*/}
+          {/*</Link>*/}
           {photo ? (
             <Image
               alt="Profile picture"
@@ -54,7 +55,7 @@ export default function Header({
       ) : (
         <Link
           className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-400 bg-blue-600 font-medium transition"
-          href="/dream"
+          href="/morse"
         >
           <p>Sign Up </p>
         </Link>
