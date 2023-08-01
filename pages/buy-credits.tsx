1d6bfcf8dbb7afbad1457ba9ed279fec74ac7241
gychoi <gychoi@gatolab.com>
2023-08-01 14:02:09 +0900

    크래딧 페이지 수정

--- pages/buy-credits.tsx
+++ pages/buy-credits.tsx
@@ -51,110 +51,110 @@ export default function Pricing() {
           />
         )}
       </div>
-      <div className="mt-10 text-center">
-        <h4 className="flex-none leading-6 mt-2 text-2xl font-bold tracking-tight text-white sm:text-5xl">
-          What’s included
-        </h4>
-      </div>
-      <ul
-        role="list"
-        className="mt-8 grid grid-cols-1 gap-4 leading-6 text-gray-400 sm:grid-cols-2 sm:gap-6 mb-10"
-      >
-        <li className="flex gap-x-3">
-          <svg
-            className="h-6 w-5 flex-none text-blue-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Save your generated rooms
-        </li>
+      {/*<div className="mt-10 text-center">*/}
+      {/*  <h4 className="flex-none leading-6 mt-2 text-2xl font-bold tracking-tight text-white sm:text-5xl">*/}
+      {/*    What’s included*/}
+      {/*  </h4>*/}
+      {/*</div>*/}
+      {/*<ul*/}
+      {/*  role="list"*/}
+      {/*  className="mt-8 grid grid-cols-1 gap-4 leading-6 text-gray-400 sm:grid-cols-2 sm:gap-6 mb-10"*/}
+      {/*>*/}
+      {/*  <li className="flex gap-x-3">*/}
+      {/*    <svg*/}
+      {/*      className="h-6 w-5 flex-none text-blue-500"*/}
+      {/*      viewBox="0 0 20 20"*/}
+      {/*      fill="currentColor"*/}
+      {/*      aria-hidden="true"*/}
+      {/*    >*/}
+      {/*      <path*/}
+      {/*        fill-rule="evenodd"*/}
+      {/*        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"*/}
+      {/*        clip-rule="evenodd"*/}
+      {/*      />*/}
+      {/*    </svg>*/}
+      {/*    Save your generated rooms*/}
+      {/*  </li>*/}
 
-        <li className="flex gap-x-3">
-          <svg
-            className="h-6 w-5 flex-none text-blue-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Get more room styles and room types
-        </li>
+      {/*  <li className="flex gap-x-3">*/}
+      {/*    <svg*/}
+      {/*      className="h-6 w-5 flex-none text-blue-500"*/}
+      {/*      viewBox="0 0 20 20"*/}
+      {/*      fill="currentColor"*/}
+      {/*      aria-hidden="true"*/}
+      {/*    >*/}
+      {/*      <path*/}
+      {/*        fill-rule="evenodd"*/}
+      {/*        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"*/}
+      {/*        clip-rule="evenodd"*/}
+      {/*      />*/}
+      {/*    </svg>*/}
+      {/*    Get more room styles and room types*/}
+      {/*  </li>*/}
 
-        <li className="flex gap-x-3">
-          <svg
-            className="h-6 w-5 flex-none text-blue-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Premium support by email
-        </li>
+      {/*  <li className="flex gap-x-3">*/}
+      {/*    <svg*/}
+      {/*      className="h-6 w-5 flex-none text-blue-500"*/}
+      {/*      viewBox="0 0 20 20"*/}
+      {/*      fill="currentColor"*/}
+      {/*      aria-hidden="true"*/}
+      {/*    >*/}
+      {/*      <path*/}
+      {/*        fill-rule="evenodd"*/}
+      {/*        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"*/}
+      {/*        clip-rule="evenodd"*/}
+      {/*      />*/}
+      {/*    </svg>*/}
+      {/*    Premium support by email*/}
+      {/*  </li>*/}
 
-        <li className="flex gap-x-3">
-          <svg
-            className="h-6 w-5 flex-none text-blue-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Ability to request features
-        </li>
-        <li className="flex gap-x-3">
-          <svg
-            className="h-6 w-5 flex-none text-blue-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Commercial usage of photos
-        </li>
-        <li className="flex gap-x-3">
-          <svg
-            className="h-6 w-5 flex-none text-blue-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Early access to new features
-        </li>
-      </ul>
-      <p className="text-gray-400 mb-5">
+      {/*  <li className="flex gap-x-3">*/}
+      {/*    <svg*/}
+      {/*      className="h-6 w-5 flex-none text-blue-500"*/}
+      {/*      viewBox="0 0 20 20"*/}
+      {/*      fill="currentColor"*/}
+      {/*      aria-hidden="true"*/}
+      {/*    >*/}
+      {/*      <path*/}
+      {/*        fill-rule="evenodd"*/}
+      {/*        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"*/}
+      {/*        clip-rule="evenodd"*/}
+      {/*      />*/}
+      {/*    </svg>*/}
+      {/*    Ability to request features*/}
+      {/*  </li>*/}
+      {/*  <li className="flex gap-x-3">*/}
+      {/*    <svg*/}
+      {/*      className="h-6 w-5 flex-none text-blue-500"*/}
+      {/*      viewBox="0 0 20 20"*/}
+      {/*      fill="currentColor"*/}
+      {/*      aria-hidden="true"*/}
+      {/*    >*/}
+      {/*      <path*/}
+      {/*        fill-rule="evenodd"*/}
+      {/*        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"*/}
+      {/*        clip-rule="evenodd"*/}
+      {/*      />*/}
+      {/*    </svg>*/}
+      {/*    Commercial usage of photos*/}
+      {/*  </li>*/}
+      {/*  <li className="flex gap-x-3">*/}
+      {/*    <svg*/}
+      {/*      className="h-6 w-5 flex-none text-blue-500"*/}
+      {/*      viewBox="0 0 20 20"*/}
+      {/*      fill="currentColor"*/}
+      {/*      aria-hidden="true"*/}
+      {/*    >*/}
+      {/*      <path*/}
+      {/*        fill-rule="evenodd"*/}
+      {/*        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"*/}
+      {/*        clip-rule="evenodd"*/}
+      {/*      />*/}
+      {/*    </svg>*/}
+      {/*    Early access to new features*/}
+      {/*  </li>*/}
+      {/*</ul>*/}
+      <p className="text-gray-400 mb-5 mt-10">
         Interested in team pricing or have any pricing questions? Email{" "}
         <span className="text-gray-300">yousaidgoodbye@gmail.com</span>
       </p>
